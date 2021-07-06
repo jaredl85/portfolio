cba1567a80c36d2439eb0f4761800b3fef3d811f
jaredl85 <jaredloveridge@gmail.com>
2021-07-05 20:23:43 -0700

    added typewriter effect to splash section

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import './home.scss';
+import { init } from 'ityped';
+import { useEffect, useRef } from 'react';
 
 export default function Home() {
+
+    const textRef = useRef();
+
+    useEffect(() => {
+        init(textRef.current, { 
+            showCursor: true,
+            typeSpeed: 150,
+            backDelay: 1500,
+            backSpeed: 150, 
+            strings: ['React', 'JavaScript', 'HTML', 'CSS', 'BootStrap', 'SASS'] 
+        });
+    }, []);
+
     return (
         <div className="home" id="home">
             <div className="splash-text-box">
                 <h1>
                     Hello. I'm <span>Jared.</span>
                     <br />
-                    I build websites in <span>$tech.</span>
+                    I build websites with <span ref={textRef}></span>
                 </h1>
                 <br/>
                 <a className="home-btn" href="#work">See my work</a>
